9f5c22ccbbc139578e84bd7a95be2cac13f81760
applefrittr <jacob.lee.jordan@gmail.com>
2024-03-26 18:07:19 -0400

    initial commit, create-react-app cleanup, socket.io installed and connects to API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import socket from "./API/socket";
 
 function App() {
+  useEffect(() => {
+    socket.connect();
+
+    socket.on("connect", () => {
+      console.log("connected to server!");
+    });
+
+    socket.on("new message", (message) => {
+      console.log("new message recieved from Discord:", message);
+    });
+
+    socket.on("get channels", (channels) => {
+      console.log(channels);
+    });
+
+    return () => {
+      console.log("disconnecting...");
+      socket.off("new message");
+      socket.off("get channels");
+      socket.disconnect();
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Discognito Client</header>
     </div>
   );
 }
