9105eeb7cc179cd768c57322b0602c1c5f9b0072
applefrittr <jacob.lee.jordan@gmail.com>
2025-04-28 17:55:04 -0400

    switched out BrowserRouter for HashRouter in index.js, updated RouteSwitch

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,14 +1,12 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Landing from "./Landing";
 import App from "./App";
 
 function RouteSwitch() {
-  const location = useLocation();
-
   return (
     <AnimatePresence mode="wait">
-      <Routes key={location.pathname} location={location}>
+      <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/client" element={<App />} />
       </Routes>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import RouteSwitch from "./components/RouteSwitch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={"/discognito-app"}>
+    <HashRouter>
       <RouteSwitch />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
